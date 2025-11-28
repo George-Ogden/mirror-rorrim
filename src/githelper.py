b410e44280039edb8f1b552eae24b864954b6ed5
George Ogden <george-ogden@outlook.com>
2025-11-28 17:00:55 +0000

    Use os.fspath instead of __fspath__ for getting string filepaths

--- src/githelper.py
+++ src/githelper.py
@@ -1,4 +1,5 @@
 import functools
+import os
 import shutil
 
 import git
@@ -25,11 +26,11 @@ class GitHelper:
     @classmethod
     def _clone(cls, remote: Remote, local: AbsDir) -> None:
         # Convert to string explicitly to gitpython-developers/GitPython#2085
-        git.Repo.clone_from(remote.__fspath__(), local.__fspath__())
+        git.Repo.clone_from(os.fspath(remote), os.fspath(local))
 
     @classmethod
     def _sync(cls, local: AbsDir) -> None:
         # gitpython-developers/GitPython#2085
-        repo = git.Repo(local.__fspath__())
+        repo = git.Repo(os.fspath(local))
         [fetch_info] = repo.remote().fetch()
         repo.head.reset(fetch_info.commit, working_tree=True, index=True)
